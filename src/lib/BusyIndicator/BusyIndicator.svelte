0a11951c02ea8a02a9b32bd08f5aa28ccbb3f3de
Edward Smyth <git@edwardsmyth.com>
2022-06-30 19:53:14 -0700

    Updated busy indicator props set-up

--- src/lib/BusyIndicator/BusyIndicator.svelte
+++ src/lib/BusyIndicator/BusyIndicator.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import '@fundamental-styles/fn/dist/fn-busy-indicator.css';
+
+	$: props = {
+		...$$restProps,
+		class: ['fn-busy-indicator', $$restProps.class].filter(Boolean).join(' ')
+	};
 </script>
 
-<div class="fn-busy-indicator" {...$$restProps}>
+<div {...props}>
 	<div class="fn-busy-indicator--circle-0" />
 	<div class="fn-busy-indicator--circle-1" />
 	<div class="fn-busy-indicator--circle-2" />
