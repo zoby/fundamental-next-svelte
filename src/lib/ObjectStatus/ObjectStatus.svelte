59f9d70b38ca93e9c030f64c868456671c64c5ff
Edward Smyth <git@edwardsmyth.com>
2022-07-02 16:34:04 -0700

    Added object status component

---
+++ src/lib/ObjectStatus/ObjectStatus.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import '@fundamental-styles/fn/dist/fn-object-status.css';
+
+	/**
+	 * Specify the kind of button
+	 * @type {"info" | "positive" | "critical" | "negative"}
+	 * @required
+	 */
+	export let state: string;
+
+	/**
+	 * Set to true for interactive object status
+	 * @type {boolean}
+	 */
+	export let interactive: boolean = false;
+
+	$: props = {
+		...$$restProps,
+		class: [
+			'fn-object-status',
+			interactive && 'fn-object-status--interactive',
+			['info', 'positive', 'critical', 'negative'].indexOf(state) > -1 &&
+				`fn-object-status--${state}`,
+			$$restProps.class
+		]
+			.filter(Boolean)
+			.join(' ')
+	};
+
+	$: iconClass = `sap-icon sap-icon--status-${state}`;
+</script>
+
+<div {...props} on:click on:mouseover on:mouseenter on:mouseleave on:focus>
+	<span class={iconClass} />
+	<div class="fn-object-status__text"><slot /></div>
+</div>
---
+++ src/stories/components/ObjectStatus.stories.svelte
@@ -0,0 +1,76 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import { ObjectStatus } from '$lib';
+
+	const argTypes = {
+		state: {
+			options: ['info', 'positive', 'critical', 'negative'],
+			control: { type: 'select' }
+		},
+		slot_default: {
+			name: 'default',
+			description: 'Set the object status value',
+			control: false
+		},
+		event_click: {
+			name: 'click',
+			control: false
+		},
+		event_mouseover: {
+			name: 'mouseover',
+			control: false
+		},
+		event_mouseenter: {
+			name: 'mouseenter',
+			control: false
+		},
+		event_mouseleave: {
+			name: 'mouseleave',
+			control: false
+		},
+		event_focus: {
+			name: 'focus',
+			control: false
+		}
+	};
+	const parameters = {
+		docs: {
+			description: {
+				component: "	import { ObjectStatus } from 'fundamental-next-svelte';"
+			}
+		}
+	};
+</script>
+
+<Meta title="Components/Object Status" component={ObjectStatus} {argTypes} {parameters} />
+
+<Template let:args>
+	<ObjectStatus {...args}>Status</ObjectStatus>
+</Template>
+
+<Story name="Default" source />
+
+<Story name="Interactive">
+	<div>
+		<ObjectStatus state="positive" interactive>Positive</ObjectStatus>
+		<ObjectStatus state="critical" interactive>Critical</ObjectStatus>
+		<ObjectStatus state="negative" interactive>Negative</ObjectStatus>
+		<ObjectStatus state="info" interactive>Info</ObjectStatus>
+	</div>
+</Story>
+
+<Story name="Non-Interactive">
+	<div>
+		<ObjectStatus state="positive">Positive</ObjectStatus>
+		<ObjectStatus state="critical">Critical</ObjectStatus>
+		<ObjectStatus state="negative">Negative</ObjectStatus>
+		<ObjectStatus state="info">Info</ObjectStatus>
+	</div>
+</Story>
+
+<style>
+	div {
+		display: flex;
+		gap: 20px;
+	}
+</style>
