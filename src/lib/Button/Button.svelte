001a4d7aa568734fbed89146af471faf1c4217cf
Edward Smyth <git@edwardsmyth.com>
2022-07-04 23:58:25 -0700

    Updated icon implementation for button component

--- src/lib/Button/Button.svelte
+++ src/lib/Button/Button.svelte
@@ -37,6 +37,12 @@
 	 */
 	export let iconBeforeText: boolean = true;
 
+	/**
+	 * Specify the button icon
+	 * @type {"accelerated" | "accept" | "accidental-leave" | "account" | "accounting-document-verification" | "activity-individual" | "activity-items" | "add-activity" | "add-contact" | "add-folder" | "add-photo" | "add-process" | "add-product" | "add" | "address-book" | "appointment-2" | "appointment" | "approvals" | "area-chart" | "arobase" | "attachment-audio" | "attachment-epub" | "attachment-html" | "attachment-photo" | "attachment" | "background" | "badge" | "bar-chart" | "barcode" | "basket" | "bell" | "blank-tag-2" | "blank-tag" | "blur" | "bo-strategy-management" | "bubble-chart" | "building" | "bullet-text" | "burglary" | "bus-public-transport" | "business-one" | "busy" | "calendar" | "call" | "camera" | "card" | "cargo-train" | "cart-2" | "cart-3" | "cart" | "chain-link" | "chalkboard" | "chart-axis" | "chart-table-view" | "chart-tree-map" | "chevron-phase-2" | "chevron-phase" | "choropleth-chart" | "circle-task-2" | "circle-task" | "cloud" | "co" | "collaborate" | "collapse-all" | "collapse" | "column-chart-dual-axis" | "comment" | "commission-check" | "company-view" | "compare" | "copy" | "course-book" | "course-program" | "create 1" | "create" | "crm-service-manager" | "crop" | "crossed-line-chart" | "curriculum" | "cursor-arrow" | "customer-history" | "customer-order-entry" | "customer-view" | "customize" | "database" | "desktop-mobile" | "detail-view" | "developer-settings" | "dimension" | "disconnected" | "doc-attachment" | "doctor" | "document-text" | "document" | "documents" | "drill-down" | "drill-up" | "drop-down-list" | "dropdown" | "duplicate" | "education" | "electrocardiogram" | "electronic-medical-record" | "email-read" | "email" | "end-user-experience-monitoring" | "endoscopy" | "energy-saving-lightbulb" | "enter-more" | "eraser" | "expand-all" | "expand-group" | "expense-report" | "explorer" | "factory" | "fax-machine" | "feed" | "feeder-arrow" | "filter-analytics" | "filter" | "folder-blank" | "folder-full" | "folder" | "form" | "forward" | "future" | "fx" | "gantt-bars" | "general-leave-request" | "generate-shortcut" | "group-2" | "group" | "header" | "heading1" | "heading2" | "hide" | "hint" | "history" | "home-share" | "home" | "horizontal-stacked-chart" | "horizontal-waterfall-chart" | "hr-approval" | "iPad-2" | "iPad" | "iPhone-2" | "iPhone" | "idea-wall" | "image-viewer" | "initiative" | "inspect-down" | "inspect" | "inspection" | "instance" | "it-host" | "journey-arrive" | "journey-change" | "journey-depart" | "key-user-setting" | "key" | "lead-outdated" | "lead" | "leades" | "learning-assistant" | "legend" | "line-chart-time-axis" | "line-charts" | "list" | "loan" | "locate-me" | "map-2" | "map-3" | "map" | "master-task-triangle" | "media-forward" | "media-pause" | "media-play" | "media-reverse" | "media-rewind" | "megaphone" | "message-popup" | "message-success" | "message-warning" | "microphone" | "mileage" | "move" | "mri-scan" | "multi-select" | "multiple-bar-chart" | "multiple-line-chart" | "my-view" | "nav-back" | "navigation-down-arrow" | "navigation-left-arrow" | "navigation-right-arrow" | "notification-2" | "notification" | "number 1" | "number" | "nurse" | "opportunities" | "opportunity" | "order-status" | "org-chart" | "outbox" | "paging" | "paid-leave" | "palette" | "paper-plane" | "passenger-train" | "pdf-reader" | "pending" | "per-diem" | "performance" | "permission" | "physical-activity" | "picture" | "pie-chart" | "pipeline-analysis" | "pixelate" | "ppt-attachment" | "present" | "primary-key" | "print" | "private" | "project-definition-triangle-2" | "projector" | "provision" | "pull-down" | "pushpin-off" | "receipt" | "record" | "redo 1" | "redo" | "repost" | "resize-vertical" | "response" | "responsive 1" | "responsive" | "retail-store" | "sales-notification" | "sales-order-item" | "sales-order" | "sales-quote" | "sap-box" | "screen-split-one" | "screen-split-three" | "screen-split-two" | "search" | "settings" | "shortcut" | "show-edit" | "show" | "simple-payment" | "simulate" | "sonography" | "sort-ascending" | "sort-descending" | "sort" | "sorting-ranking" | "status-critical" | "status-error" | "status-in-process" | "status-inactive" | "status-negative" | "study-leave" | "subway-train" | "suitcase" | "supplier" | "survey" | "sys-add" | "sys-back-2" | "sys-back" | "sys-cancel-2" | "sys-cancel" | "sys-help-2" | "sys-help" | "sys-last-page" | "sys-minus" | "sys-monitor" | "table-column" | "table-row" | "table-view" | "tag-cloud-chart" | "tag" | "temperature" | "text-align-center" | "text-align-justified" | "text-align-left" | "text" | "thumb-down" | "thumb-up" | "time-account" | "time-entry-request" | "time-overdue" | "tools-opportunity" | "touch" | "travel-expense-report" | "travel-expense" | "travel-itinerary" | "two-keys" | "ui-notifications" | "umbrella" | "underline-text" | "undo" | "upload-to-cloud" | "upload" | "upstacked-chart" | "user-edit" | "user-settings" | "vertical-bar-chart-2" | "vertical-bullet-chart" | "vertical-grip" | "vertical-stacked-chart" | "vertical-waterfall-chart" | "warning-2" | "washing-machine" | "weather-proofing" | "web-cam" | "widgets"}
+	 */
+	export let icon: string = '';
+
 	$: props = {
 		...$$restProps,
 		class: [
@@ -46,7 +52,7 @@
 			['positive', 'critical', 'negative'].indexOf(state) > -1 && `fn-button--${state}`,
 			layout && 'fn-button--layout',
 			selected && 'fn-button--selected',
-			$$slots.icon && !$$slots.default && 'fn-button--icon-only',
+			icon && !$$slots.default && 'fn-button--icon-only',
 			$$restProps.class
 		]
 			.filter(Boolean)
@@ -55,11 +61,13 @@
 </script>
 
 <button {...props} on:click on:mouseover on:mouseenter on:mouseleave on:focus>
-	{#if iconBeforeText}
-		<slot name="icon" />
+	{#if iconBeforeText && icon}
+		<span class={`sap-icon sap-icon--${icon}`} />
 	{/if}
-	<span class="fn-button__text"><slot /></span>
-	{#if !iconBeforeText}
-		<slot name="icon" />
+	{#if $$slots.default}
+		<span class="fn-button__text"><slot /></span>
+	{/if}
+	{#if !iconBeforeText && icon}
+		<span class={`sap-icon sap-icon--${icon}`} />
 	{/if}
 </button>
